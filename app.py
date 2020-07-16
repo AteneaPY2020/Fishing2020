f920b20b82b415ba1a8eaaf6f5b2bcf948a2bd9d
Jakelin Rivera <60167444+Jakelin-prog@users.noreply.github.com>
2020-07-15 19:52:34 -0600

    html de logIn y registro

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/logIn")
+def logIn():
+    return render_template("logIn.html")
+
+
+@app.route("/signUpInversor")
+def signUpInversor():
+    return render_template("signUpInversor.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)

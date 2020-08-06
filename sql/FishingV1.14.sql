d163d385d8eb3cf88a69a042f8a75495fe22d39c
Sergio <chejo.lopez2015.cl@gmail.com>
2020-08-06 08:48:15 -0600

    arreglo de una cosa

--- sql/FishingV1.14.sql
+++ sql/FishingV1.14.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `fishingdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `fishingdb`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: fishingdb
@@ -418,4 +416,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-06  8:30:56
+-- Dump completed on 2020-08-06  8:47:02
